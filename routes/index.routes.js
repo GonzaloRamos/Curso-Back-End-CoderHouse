2c55ce7a6549f1b5f9fb5b1d21bd0807700b625a
Gonzalo Ramos <gonzalo.ariel13@gmail.com>
2022-01-27 01:07:52 -0300

    Bug Fix de routes

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -10,17 +10,15 @@ router.use(express.json());
 router.use(express.urlencoded({extended: true}));
 
 //Rutas
-router.use("*", (req, res) => {
-  res
-    .status(404)
-    .json({
-      error: -2,
-      descripcion: `La ruta ${req.baseUrl} con el metodo ${req.method} no esta implementado`,
-    });
-});
 router.use("/api/carrito", routeCarrito);
 router.use("/api/productos", routeProductosPublic);
 router.use("/api/productos", authMiddleware, routerProductosPrivate);
+router.use("*", (req, res) => {
+  res.status(404).json({
+    error: -2,
+    descripcion: `La ruta ${req.baseUrl} con el metodo ${req.method} no esta implementado`,
+  });
+});
 
 router.get("/", (req, res) => {
   res.send("HOLAAA");
