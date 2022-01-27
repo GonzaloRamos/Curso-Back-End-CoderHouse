589cd120c7f46d6571422579b4854d50e25a0069
Gonzalo Ramos <gonzalo.ariel13@gmail.com>
2022-01-26 22:51:46 -0300

    Add validacion de rutas no implementadas

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { authMiddleware } from "../middlewares/authMiddleware.js";
+import {authMiddleware} from "../middlewares/authMiddleware.js";
 import routeCarrito from "./carrito/carrito.routes.js";
 import routerProductosPrivate from "./productos/private/productosPrivate.routes.js";
 import routeProductosPublic from "./productos/public/productosPublic.routes.js";
@@ -7,9 +7,17 @@ import routeProductosPublic from "./productos/public/productosPublic.routes.js";
 const router = express.Router();
 
 router.use(express.json());
-router.use(express.urlencoded({ extended: true }));
+router.use(express.urlencoded({extended: true}));
 
 //Rutas
+router.use("*", (req, res) => {
+  res
+    .status(404)
+    .json({
+      error: -2,
+      descripcion: `La ruta ${req.baseUrl} con el metodo ${req.method} no esta implementado`,
+    });
+});
 router.use("/api/carrito", routeCarrito);
 router.use("/api/productos", routeProductosPublic);
 router.use("/api/productos", authMiddleware, routerProductosPrivate);
