2bfebae5caf0c33f8cbfe9a28b8f5e52c0f3f35c
Gonzalo Ramos <gonzalo.ariel13@gmail.com>
2022-04-12 19:29:45 -0300

    fix port bug heroku

--- config/configServer.js
+++ config/configServer.js
@@ -1,9 +1,8 @@
-console.log(process.env);
 const yargs = require("yargs/yargs");
 const {hideBin} = require("yargs/helpers");
 const argv = yargs(hideBin(process.argv)).argv;
 
-const PORT = argv.port ? argv.port : 8080;
+const PORT = argv.port ? argv.port : process.env.PORT || 8080;
 const MODE = argv.mode === "cluster" ? argv.mode : "fork";
 
 module.exports = {PORT, MODE};
